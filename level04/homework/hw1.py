c0a0e47b8946302913f1d50152c6c8c2dc3632cd
sabasas <jt209356@gmail.com>
2024-10-27 21:33:39 +0400

    second commit

---
+++ level04/homework/hw1.py
@@ -0,0 +1,10 @@
+# დაადეკლარირეთ(შექმენით) 3 ინტეჯერ ტიპის ცვლადი სადაც შეინახავთ მთელ რიცხვებს და დაბეჭდავთ(print) მათ ჯამს
+
+num1 = 40
+num2 = 100
+num3 = 70
+
+print(num1 + num2 + num3)
+print(num3 - num1 - num2)
+print(num2 * num1 * num3)
+print(num2 / num1 / num3)
