733a626cbe09f76a7eae6e965e4da068a9535b00
sabasas <jt209356@gmail.com>
2024-11-03 16:05:56 +0400

    second commit

---
+++ level08/homework/hw2.py
@@ -0,0 +1,11 @@
+# value = "10"
+# result = value * 5
+# value = "10"
+# print("Result:", result)
+
+# გაიხსენეთ მონაცემთა ტიპები და შეასწორეთ ეს კოდი 
+
+
+value ="10"
+result =  int (value) * 5
+print("Result:", result)
---
+++ level08/homework/hw5.py
@@ -0,0 +1,7 @@
+num1 = 6
+num2 = 8
+num3 = 10
+average = (num1 + num2 + num3) / 3
+print("Average:", str (average))
+
+# შეასწორეთ კოდი ისე რომ საშუალო არითმეტიკული სწორად გამოითვალოს
