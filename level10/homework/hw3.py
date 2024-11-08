0a6dace341f476df8252efaec762e122e86c7ae3
sabasas <jt209356@gmail.com>
2024-11-08 18:52:20 +0400

    second commit

---
+++ level10/homework/hw3.py
@@ -0,0 +1,6 @@
+# დაწერე პროგრამა, რომელიც შემოატანინებს მომხმარებელს ორი რიცხვს და დაბეჭდავს მათ სხვაობას.
+
+num =  int (input("your age:"))
+num1 =  int(input("your friend age:"))
+print(num - num1)
+    
