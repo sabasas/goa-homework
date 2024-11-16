3ec95e372c757ec79850f1a64d0ab23259fce5f5
sabasas <jt209356@gmail.com>
2024-11-17 00:39:13 +0400

    second commit

---
+++ level13/homework/hw1.py
@@ -0,0 +1,11 @@
+# მომხმარებელს შემოატაინეთ ორი რიცხვი და მათზე მოახდინეთ მათემატიკური ოპერაციები.
+
+first_number = int (input("ჩაწერე რიცხვი:"))
+second_number = int(input("ჩაწერე მე-2 რიცხვი:"))
+
+print(first_number + second_number)
+print(first_number - second_number)
+print(first_number * second_number)
+print(first_number / second_number)
+print(first_number % second_number)
+print(first_number ** second_number)
---
+++ level13/homework/hw6.py
@@ -0,0 +1,12 @@
+#  მომხმარებელს შემოატაინეთ 2 რიცხვი, და შემდეგ გამოიყენეთ შედარების ოპერატორები რა რიცხვსაც შეიყვანს მომხმარებელი,
+#  მასე გამოიყენთ შედარების ოპერატორები ==,!=,<,>,<=,=>.
+
+first_number = int(input("შეიყვანეთ რიცხვი:"))
+second_number = int(input("შეიყვანეთ მე-2 რიცხვი:"))
+
+print(first_number == second_number)
+print(first_number != second_number)
+print(first_number < second_number)
+print(first_number > second_number)
+print(first_number <= second_number)
+print(first_number >= second_number)
